304c5a53da5d557fd8fc65ee819f2d92434f8175
”Zakirov <barbathrummmusmarat@gmail.com>
2021-07-10 11:57:30 +0300

    feat: update ip adress

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ http
   .createServer(function (request, response) {
     response.end('Hello NodeJS!');
   })
-  .listen(3000, 'localhost', function () {
+  .listen(3000, '146.158.12.225', function () {
     console.log('Сервер начал прослушивание запросов на порту 3000');
   });
